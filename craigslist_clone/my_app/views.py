a22cc7f292aa41ede6fb9f980e7a8abbfeab5062
MaxHarlan206 <maximuscollinge@gmail.com>
2021-02-12 13:26:28 -0800

    Scraping craigslist for: image, title, url, and price

--- craigslist_clone/my_app/views.py
+++ craigslist_clone/my_app/views.py
@@ -4,10 +4,12 @@ from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
 from . import models
 
-cl_base_url = "https://seattle.craigslist.org/d/services/search/bbb?query={}"
+cl_base_url = "https://seattle.craigslist.org/search/?query={}"
+BASE_IMAGE_URL = "https://images.craigslist.org/{}_300x300.jpg"
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
 
 def new_search(request):
     search = request.POST.get('search')
@@ -17,8 +19,35 @@ def new_search(request):
     response = requests.get(final_url)
     # gets all of the html and css from the queried url to parse
     data = response.text
-    print(data)
+    soup = BeautifulSoup(data, features="html.parser")
+
+    # creates a list of items of all
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+    final_postings = []
+    for post in post_listings:
+        post_title = post.find(class_='result-heading').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price') is not None:
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = "N/A"
+
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+    for val in final_postings:
+        print(val)
+
     frontend_elements = {
         'search': search,
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html', frontend_elements)
